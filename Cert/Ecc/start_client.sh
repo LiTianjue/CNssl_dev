8204e5de31996cad7a35067cabc92b1f6e3d6c8d
LiTianjue <mhpmii@126.com>
2016-07-07 17:44:13 +0800

    添加测试证书

--- Cert/Ecc/start_client.sh
+++ Cert/Ecc/start_client.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-openssl s_client -CAfile ca.crt -cert client.crt -key client.key -connect 127.0.0.1:4433 -debug
+#openssl s_client -tls1_1 -CAfile ca.crt -cert client.crt -key client.key -connect 127.0.0.1:4433 -debug
+
+openssl s_client -tls1_1 -connect 127.0.0.1:4433 -debug
--- Cert/Ecc/start_server.sh
+++ Cert/Ecc/start_server.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-openssl s_server -CAfile ca.crt -cert server.crt -key server.key -debug
+#openssl s_server -tls1_1 -CAfile ca.crt -cert server.crt -key server.key -debug
+openssl s_server -tls1_1 -CAfile ca.crt -cert server.crt -key server.key -debug
